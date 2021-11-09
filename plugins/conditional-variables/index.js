98a0d23ba1250fce6d511cef953576b97e49edf2
Michael Cowie <mcowie@quebit.com>
2021-11-09 14:59:49 -0500

    First working cut of plugin.

--- plugins/conditional-variables/index.js
+++ plugins/conditional-variables/index.js
@@ -1,27 +1,38 @@
 module.exports.templateTags = [
   {
-    name: 'conditionalVariable',
-    displayName: 'ConditionalVariable',
-    description: 'conditional evaluation of variables',
+    name: "conditionalVariable",
+    displayName: "ConditionalVariable",
+    description: "returns one of two values based on a true/false expression",
     args: [
       {
-        displayName: 'Expression',
-        type: 'string',
-		placeholder: 'true'
+        displayName: "Expression to Evaluate",
+        type: "string",
+        placeholder: "\"ABC\" === \"DEF\""
       },
       {
-        displayName: 'Value when True',
-        type: 'string',
-        placeholder: 'True Result',
+        displayName: "Value when True",
+        type: "string",
+        placeholder: "it\'s true!",
       },
       {
-        displayName: 'Value when False',
-        type: 'string',
-        placeholder: 'False Result',
+        displayName: "Value when False",
+        type: "string",
+        placeholder: "it\'s false!",
       },
     ],
     run(context, expression, trueResult, falseResult) {
-      return eval(expression) ? trueResult : falseResult;
-    },
-  },
+      if (expression === null || expression === "")
+        throw("Missing expression!");
+      if (trueResult === null)
+        throw("Missing true result value!");
+      if (falseResult === null)
+        throw("Missing false result value!");
+      try {
+        return eval(expression) ? trueResult + "" : falseResult + "";
+      }
+      catch {
+        throw("Unable to evaluate expression!");
+      }
+    }
+  }
 ];
